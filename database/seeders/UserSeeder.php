02adcc491c1bf60f5e0e46fd2dd1937c0db95faf
bryanpercy118 <bryanpercy77@gmail.com>
2024-06-19 17:49:14 -0500

    changes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,11 +15,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // Default credentials
+        // Credenciales de Prueba
         \App\Models\User::insert([
             [ 
                 'name' => 'Left4code',
-                'email' => 'midone@left4code.com',
+                'email' => 'carlosfarith@gmail.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'gender' => 'male',
@@ -28,7 +28,6 @@ class UserSeeder extends Seeder
             ]
         ]);
 
-        // Fake users
-        User::factory()->times(9)->create();
+        
     }
 }
--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -9,7 +9,8 @@
     <h2 class="intro-y text-lg font-medium mt-10">Gestion de Area Documental</h2>
     <div class="grid grid-cols-12 gap-6 mt-5">
         <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
-            <button class="btn btn-primary shadow-md mr-2" data-tw-toggle="modal" data-tw-target="#add-confirmation-modal">Registrar Nueva Area</button>
+            <button class="btn btn-primary shadow-md mr-2" data-tw-toggle="modal"
+                data-tw-target="#add-confirmation-modal">Registrar Nueva Area</button>
         </div>
         <!-- BEGIN: Data List -->
         <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
@@ -21,72 +22,80 @@
                     </tr>
                 </thead>
                 <tbody>
-                    
-                        <tr class="intro-x">
-                            <td>
-                                <a href="" class="font-medium whitespace-nowrap">Farmacia</a>
-                            </td>
-                            
-                            <td class="table-report__action w-56">
-                                <div class="flex justify-center items-center">
-                                    <a class="flex items-center mr-3 edit-category-link"  data-tw-toggle="modal" data-tw-target="#edit-confirmation-modal" style="cursor: pointer;">
-                                        <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Editar
-                                    </a>
-                                    <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-confirmation-modal">
-                                        <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Eliminar
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr class="intro-x">
-                            <td>
-                                <a href="" class="font-medium whitespace-nowrap">Tesoreria</a>
-                            </td>
-                            
-                            <td class="table-report__action w-56">
-                                <div class="flex justify-center items-center">
-                                    <a class="flex items-center mr-3 edit-category-link"  data-tw-toggle="modal" data-tw-target="#edit-confirmation-modal" style="cursor: pointer;">
-                                        <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Editar
-                                    </a>
-                                    <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-confirmation-modal">
-                                        <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Eliminar
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr class="intro-x">
-                            <td>
-                                <a href="" class="font-medium whitespace-nowrap">Talento Humano</a>
-                            </td>
-                            
-                            <td class="table-report__action w-56">
-                                <div class="flex justify-center items-center">
-                                    <a class="flex items-center mr-3 edit-category-link"  data-tw-toggle="modal" data-tw-target="#edit-confirmation-modal" style="cursor: pointer;">
-                                        <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Editar
-                                    </a>
-                                    <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-confirmation-modal">
-                                        <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Eliminar
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr class="intro-x">
-                            <td>
-                                <a href="" class="font-medium whitespace-nowrap">Medicina General</a>
-                            </td>
-                            
-                            <td class="table-report__action w-56">
-                                <div class="flex justify-center items-center">
-                                    <a class="flex items-center mr-3 edit-category-link"  data-tw-toggle="modal" data-tw-target="#edit-confirmation-modal" style="cursor: pointer;">
-                                        <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Editar
-                                    </a>
-                                    <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-confirmation-modal">
-                                        <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Eliminar
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                 
+
+                    <tr class="intro-x">
+                        <td>
+                            <a href="" class="font-medium whitespace-nowrap">Farmacia</a>
+                        </td>
+
+                        <td class="table-report__action w-56">
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3 edit-category-link" data-tw-toggle="modal"
+                                    data-tw-target="#edit-confirmation-modal" style="cursor: pointer;">
+                                    <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Editar
+                                </a>
+                                <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal"
+                                    data-tw-target="#delete-confirmation-modal">
+                                    <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Eliminar
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="intro-x">
+                        <td>
+                            <a href="" class="font-medium whitespace-nowrap">Tesoreria</a>
+                        </td>
+
+                        <td class="table-report__action w-56">
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3 edit-category-link" data-tw-toggle="modal"
+                                    data-tw-target="#edit-confirmation-modal" style="cursor: pointer;">
+                                    <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Editar
+                                </a>
+                                <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal"
+                                    data-tw-target="#delete-confirmation-modal">
+                                    <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Eliminar
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="intro-x">
+                        <td>
+                            <a href="" class="font-medium whitespace-nowrap">Talento Humano</a>
+                        </td>
+
+                        <td class="table-report__action w-56">
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3 edit-category-link" data-tw-toggle="modal"
+                                    data-tw-target="#edit-confirmation-modal" style="cursor: pointer;">
+                                    <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Editar
+                                </a>
+                                <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal"
+                                    data-tw-target="#delete-confirmation-modal">
+                                    <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Eliminar
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="intro-x">
+                        <td>
+                            <a href="" class="font-medium whitespace-nowrap">Medicina General</a>
+                        </td>
+
+                        <td class="table-report__action w-56">
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3 edit-category-link" data-tw-toggle="modal"
+                                    data-tw-target="#edit-confirmation-modal" style="cursor: pointer;">
+                                    <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Editar
+                                </a>
+                                <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal"
+                                    data-tw-target="#delete-confirmation-modal">
+                                    <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Eliminar
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+
                 </tbody>
             </table>
         </div>
@@ -142,39 +151,38 @@
         <!-- END: Pagination -->
     </div>
 
-    
+
 
     <!-- BEGIN: add Confirmation Modal -->
     <div id="add-confirmation-modal" class="modal" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-body p-0">
-                <div class="modal-header">
-        <h5 class="modal-title" id="addCategoryModalLabel"></h5>
-        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <!-- Formulario para añadir una nueva categoría -->
-        <form action="" method="POST">
-        @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addCategoryModalLabel"></h5>
+                        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Formulario para añadir una nueva categoría -->
+                        <form action="" method="POST">
+                            @csrf
 
 
-          <!-- Campos del formulario -->
-          <div class="mb-3">
-            <label for="categoryName" class="form-label">Nombre del Area </label>
-            <input type="text" class="form-control" id="name" name="name" required>
-          </div>
-          
-          <!-- Otros campos del formulario, si los necesitas -->
-          <button type="submit" class="btn btn-primary">Añadir Nueva Area Documental</button>
-          <button type="button" data-tw-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
-        </form>
-      </div>
+                            <!-- Campos del formulario -->
+                            <div class="mb-3">
+                                <label for="categoryName" class="form-label">Nombre del Area </label>
+                                <input type="text" class="form-control" id="name" name="name" required>
+                            </div>
+
+                            <!-- Otros campos del formulario, si los necesitas -->
+                            <button type="submit" class="btn btn-primary">Añadir Nueva Area Documental</button>
+                            <button type="button" data-tw-dismiss="modal"
+                                class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- END: add Confirmation Modal -->
-
-    
 @endsection
